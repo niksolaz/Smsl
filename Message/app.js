067f2b5a748a95f0f9493c830876f1ac3515589a
niksolaz <solazzo.nicola@gmail.com>
2015-07-10 16:53:46 +0200

    added body parser

--- Message/app.js
+++ Message/app.js
@@ -16,6 +16,8 @@ var Mex = {
 	}
 };
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 app.post('/message/social',function(req,res){
