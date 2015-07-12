449b224a132af897abd924f629c66cbede69e0b1
niksolaz <solazzo.nicola@gmail.com>
2015-07-12 19:59:38 +0200

    Client GET by twitters

--- Message/app.js
+++ Message/app.js
@@ -9,32 +9,6 @@ var client = new Twitter({
 	access_token_key:process.env.Twitter_access_token_key,
 	access_token_secret:process.env.Twitter_access_token_secret
 });
-/*
-client.get('statuses/show',{id:103},function(error,tweets,response){
-	if(error) throw error;
-	console.log(tweets);
-});*/
-/*
-client.post('/statuses/update',{status:'I am Nicola'},function(error,tweet,response){
-	if(error) throw error;
-	console.log(tweet);
-});*/
-//client.stream(path, params, callback);
-
-
-var Mex = {
-	'account':{
-		'id':Number,
-		'username':String
-	},
-	'show':{
-		'message':{
-			'message_id':{
-				'social':['Twitter','Facebook','Google+']
-			}
-		}
-	}
-};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -67,37 +41,7 @@ app.get('/message/:message_id',function(req,res){
 	});
 });
 
-/*
-app.post('/message/social',function(req,res){
-	console.log(req.body.social);
-	res.json({social:req.body.social});
-});
-*
 
-
-app.post('/account',function(req,res){
-	console.log(req.body.account);
-	res.json({id:req.body.id, username:req.body.username});
-});
-
-app.get('/account/:username',function(req,res){
-	res.send(Mex[req.param.username]);
-	console.log(Mex.account.username);
-});
-
-app.put('/account/:id',function(req,res){
-});
-
-app.get('/show/message/:message_id',function(req,res){
-	res.send(Mex[req.param.message_id]);
-	console.log(Mex.show.message.message_id);
-});
-
-app.get('/show/message/message_id/:social',function(req,res){
-	res.send(Mex[req.param.social]);
-	console.log(Mex.show.message.message_id.social);
-});
-*/
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
