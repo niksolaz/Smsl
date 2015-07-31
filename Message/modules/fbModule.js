1f0227dc117f594a016de3ac6a045f6a15d3b03c
niksolaz <solazzo.nicola@gmail.com>
2015-07-31 10:31:41 +0200

    basic module facebook

--- Message/modules/fbModule.js
+++ Message/modules/fbModule.js
@@ -10,4 +10,33 @@ facebook.userfb = function(){
 	return userfb;
 };
 
+facebook.get = function(mongoObj,tweets,callback){
+					FB.setAccessToken(process.env.ACCESS_TOKEN);
+					FB.api(mongoObj.fb_id,{posts:[{'id':mongoObj.fb_id}],accessToken:process.env.ACCESS_TOKEN}, function (res) {
+	  					if(!res || res.error) {
+	   						console.log(!res ? 'error occurred' : res.error);
+	   						console.log('Error posting Facebook');
+	    					callback(true,res.error);
+	   						return;
+	  					}
+	  					console.log('From Facebook: '+res);
+	  					callback(null,mongoObj,tweets,res);
+					});
+};
+
+facebook.post = function(tweet,callback){
+			
+					FB.setAccessToken(process.env.ACCESS_TOKEN);
+					FB.api('me/feed', 'post',{message:msg}, function (res) {
+						if(!res || res.error) {
+							console.log(!res ? 'error occurred' : res.error);
+		    				console.log('Error posting Facebook');
+		    				callback(true,res.error);
+		    				return;
+		  				}  					
+		  				console.log('Post Id: ' + res.id);
+		  				callback(null,tweet,res)
+					});
+};
+
 module.exports = facebook;
--- Message/modules/twModule.js
+++ Message/modules/twModule.js
@@ -3,6 +3,13 @@ var express = require('express');
 var Twitter = require('twitter');
 var database = require('./dbModule');
 
+var client = new Twitter({
+	consumer_key:process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret:process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key:process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret:process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 twitter.userTweet = function(){
 	var userTweet = process.env.USER_TW_ID;
 	console.log('USER_TW_ID',userTweet);
