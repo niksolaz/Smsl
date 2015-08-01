1cf7f9f488846da8b7cf81a1410285275ea911b4
Solazzo Nicola <solazzo.nicola@gmail.com>
2015-08-01 14:53:35 +0000

    Refactoring of the facebook module

--- Message/modules/fbModule.js
+++ Message/modules/fbModule.js
@@ -1,41 +1,76 @@
-
-var express = require('express');
 var FB = require('fb');
-var dbModule = require('./dbModule');
-var twModule = require('./twModule');
 
+// Access token
+var accessToken = process.env.ACCESS_TOKEN;
 
 module.exports.userfb = function(){
+	
 	var userfb =process.env.USER_FB_ID;
-	console.log('USER_FB_ID',userfb);
+	
+	console.log('(Facebook) UserId: ', userfb );
 	return userfb;
 };
 
-module.exports.get = function(mongoObj,tweets,callback){
-					FB.setAccessToken(process.env.ACCESS_TOKEN);
-					FB.api(mongoObj.fb_id,{posts:[{'id':mongoObj.fb_id}],accessToken:process.env.ACCESS_TOKEN}, function (res) {
-	  					if(!res || res.error) {
-	   						console.log(!res ? 'error occurred' : res.error);
-	   						console.log('Error posting Facebook');
-	    					callback(true,res.error);
-	   						return;
-	  					}
-	  					console.log('From Facebook: '+res);
-	  					callback(null,mongoObj,tweets,res);
-					});
+module.exports.get = function( facebookId, moduleCallback){
+	// Result that will be sent back
+	var result = {
+		success: false, 
+		data: null,
+		error: null
+	};
+	
+	FB.setAccessToken(accessToken);
+	
+	FB.api(
+		facebookId,
+		{
+			posts:[ {'id':facebookId} ],
+			accessToken:accessToken
+			
+		}, 
+		function (res) {
+	  		if(!res || res.error) {
+	   			console.log("(Facebook) An error has occurred. Error: ", res.error);
+	   			result.success = false;
+	   			result.error = res.error;
+	   			moduleCallback(result);
+	   			return;
+	  		}
+  			console.log('(Facebook) Facebook result: ', JSON.stringify(res) );
+  			result.success = true;
+  			result.data = res;
+  			moduleCallback(result);
+		}
+	);
 };
 
-module.exports.post = function(tweet,callback){
+module.exports.post = function ( facebookStatus, moduleCallback ){
+	// Result that will be sent back
+	var result = {
+		success: false, 
+		data: null,
+		error: null
+	};
 			
-					FB.setAccessToken(process.env.ACCESS_TOKEN);
-					FB.api('me/feed', 'post',{message:msg}, function (res) {
-						if(!res || res.error) {
-							console.log(!res ? 'error occurred' : res.error);
-		    				console.log('Error posting Facebook');
-		    				callback(true,res.error);
-		    				return;
-		  				}  					
-		  				console.log('Post Id: ' + res.id);
-		  				callback(null,tweet,res)
-					});
+	FB.setAccessToken(accessToken);
+	
+	FB.api('me/feed', 'post', { message:facebookStatus }, function ( res ) {
+		
+		if( !res || res.error ) {
+   			console.log("(Facebook) An error has occurred while posting to Facebook. Error: ", res.error);
+   			result.success = false;
+   			result.error = res.error;
+   			moduleCallback(result);
+   			return;
+  		}					
+  		console.log('(Facebook) Post Id: ' + res.id);
+  		
+  		// Format successful data
+  		result.success = true;
+  		result.data = {
+  			fb_data: res,
+  			fb_post_id: res.id
+  		};
+  		moduleCallback(result);
+	});
 };
